b72613467e9e24cdc1fc469d15b428a50aa53d6c
Weiguang Ruan <Weiguang.ruan@amlogic.com>
2022-06-17 17:13:05 +0800

    BT: provide A2DP control API via lib [1/1]
    PD#SWPL-132554
    
    Problem:
    1. upper layer want call a2dp API via lib form
    
    Solution:
    1. a2dp_ctl use lib form
    
    Verify:
    A113X
    
    Change-Id: Ie34b036184e376d07e3be76f9261c4b980e584a0
    Signed-off-by: Weiguang Ruan <Weiguang.ruan@amlogic.com>

--- utils/a2dp_ctl.h
+++ utils/a2dp_ctl.h
@@ -1,9 +1,33 @@
+// Copyright (C) 2023 Amlogic, Inc. All rights reserved.
+//
+// All information contained herein is Amlogic confidential.
+//
+// This software is provided to you pursuant to Software License
+// Agreement (SLA) with Amlogic Inc ("Amlogic"). This software may be
+// used only in accordance with the terms of this agreement.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification is strictly prohibited without prior written permission
+// from Amlogic.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #ifndef _CT_H_
 #define _CT_H_
 
 
-int player_init(void);
-void player_delinit(void);
+int a2dp_ctl_init(void);
+void a2dp_ctl_delinit(void);
 int start_play(void);
 int stop_play(void);
 int pause_play(void);
@@ -13,5 +37,15 @@ int volume_up();
 int volume_down();
 void connect_call_back(gboolean connected);
 void play_call_back(char *status);
+void set_device_mode(char *dm);
+int adapter_ready(void);
+int adapter_scan(int onoff);
+int connect_dev(const char* bddr);
+void disconnect_dev(void);
+void print_connect_status(void);
+void print_scan_results(void);
 
+int pcm_bluealsa_open(char *bddr);
+int pcm_bluealsa_close();
+int pcm_bluealsa_write(void *buf, size_t bytes) ;
 #endif
